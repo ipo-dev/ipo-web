310673b325169db5c190969bca71457c807af757
Marshall Upshur <marshall@mbucreations.com>
2019-06-24 21:55:44 -0700

    Page scaffolding
    - this contains a bunch of general page scaffolding lifted from react-material-dashboard that can serve as the core layout of app

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -4,21 +4,17 @@ import { Switch, Route, Redirect } from 'react-router-dom';
 // Views
 // import Dashboard from './views/Dashboard';
 import NotFound from './views/NotFound';
+import Account from './views/Account';
 
 export default class Routes extends Component {
     render() {
         return (
             <Switch>
-                <Redirect
+                <Route
+                    component={Account}
                     exact
-                    from="/"
-                    to="/dashboard"
+                    path="/account"
                 />
-                {/* <Route
-                    component={Dashboard}
-                    exact
-                    path="/dashboard"
-                /> */}
                 <Route
                     component={NotFound}
                     exact
---
+++ src/services/notification/index.js
@@ -0,0 +1,13 @@
+// Mock data
+let notifications = [];
+
+export const getNotifications = (limit = 6) => {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve({
+        notifications: notifications.slice(0, limit),
+        notificationsCount: notifications.length
+      });
+    }, 700);
+  });
+};
